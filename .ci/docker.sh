2610ffc1bbc752cd7628dec51f259e19e3f56337
OpenSauce04 <opensauce04@gmail.com>
2026-01-11 14:41:09 +0000

    docker: Improve git tag/ commit hash integration

--- .ci/docker.sh
+++ .ci/docker.sh
@@ -1,4 +1,15 @@
-mkdir build
+GITREV="`git show -s --format='%h'`" || true
 
-docker build -f docker/azahar-room/Dockerfile -t azahar-room .
-docker save azahar-room:latest > build/azahar-room.dockerimage
+if [ "$GITHUB_REF_TYPE" = "tag" ]; then
+    TAG_NAME=$GITHUB_REF_NAME
+elif [[ -n $GITREV ]]; then
+    TAG_NAME=$GITREV
+else
+    TAG_NAME=unknown
+fi
+
+echo "Tag name is: $TAG_NAME"
+
+docker build -f docker/azahar-room/Dockerfile -t azahar-room:$TAG_NAME .
+mkdir -p build
+docker save azahar-room:$TAG_NAME > build/azahar-room-$TAG_NAME.dockerimage
